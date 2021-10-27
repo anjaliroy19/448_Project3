6b31ce814ab562f6390e39cadd0572236bba4b17
cooper fine <c257f478@ku.edu>
2021-10-27 16:38:18 -0500

    I've added in the paddle and hitPaddle functions to the program. They work regardless of direction and the position can be easily modified if need be.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var dy;
 let wallLeftPos = 0; // these will need to be changed based on game layout
 let wallRightPos = 1500; //^^
 let wallTopPos = 0; //^^
-
+let posPaddle;
 let row = 3;
 let col = 7;
 let width = 200;
@@ -16,6 +16,9 @@ let bricks = []
 let spaceX = 5
 let spaceY = 5
 
+document.addEventListener('mousemove', e => { 
+    posPaddle = e.clientX;
+});
 
 function start(){
     x = 600;//change this to be at position above paddle
@@ -28,6 +31,7 @@ function start(){
 
 function moveBall(){
     drawBall(x,y);
+    drawPaddle(posPaddle);
     drawBricks();
     hitDetect();
     setDxDy();
@@ -42,6 +46,7 @@ function drawBall(posX, posY){
     context.arc(posX,posY,10,0, Math.PI*2, true);
     context.closePath();
     context.fill();
+    //hitPaddle(); 
 }
 
 function setDxDy(){
@@ -49,7 +54,7 @@ function setDxDy(){
     if (x <= wallLeftPos || x >= wallRightPos){
         dx = -dx;
     }
-    if (y <= wallTopPos || y > 400){ //made a bottom border for testing - remove when adding paddle
+    if (y <= wallTopPos || y > 600){ //made a bottom border for testing - remove when adding paddle
         dy = -dy;
     }
 }
@@ -83,7 +88,7 @@ function drawBricks(){
 function hitDetect(){
     for(let i = 0; i < col; i++){
         for(let j = 0; j < row; j++){
-            if(x > bricks[i][j].x && x < bricks[i][j].x + width && y > bricks[i][j].y && y < bricks[i][j].y + height){
+            if(x > bricks[i][j].x && x < bricks[i][j].x + width && y > bricks[i][j].y && y < bricks[i][j].y + height && bricks[i][j].on == 'yes'){
                 console.log('hit')
                 context.clearRect(0,0, 2000, 2000); //this may need to be changed depending on the defined canvas width and height
                 dy = -dy
@@ -94,6 +99,41 @@ function hitDetect(){
     }
 }
 
+function drawPaddle() {
+  let posR = posPaddle;
+  if(posR => 50 && posR <= 1450) {
+    context.beginPath();
+    context.fillStyle = 'Blue';
+    context.fillRect(posR-50, 550, 100, 10);
+    context.closePath();
+    console.log(posR);
+  }
+  if(posR < 50) {
+    posPaddle=50;
+    context.beginPath();
+    context.fillStyle = 'Blue';
+    context.fillRect(0, 550, 100, 10);
+    context.closePath();
+    console.log(posR);
+  }
+  if(posR > 1450) {
+    posPaddle=1450;
+    context.beginPath();
+    context.fillStyle = 'Blue';
+    context.fillRect(1400, 550, 100, 10);
+    context.closePath();
+    console.log(posR);
+  }
+  hitPaddle();
+}
+
+function hitPaddle() {
+  if((y == 550) && (x > posPaddle-50) && (x < posPaddle+50)) {
+    console.log("paddleHit");
+    dy = -dy;
+  }
+
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     canvas = document.querySelector("#projectCanvas");
